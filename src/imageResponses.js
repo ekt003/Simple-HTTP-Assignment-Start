7bbcf6f0edc38f176a086b890b49ecdc201365be
Emily Turner (RIT Student) <ekt6170@ad.rit.edu>
2019-09-05 16:33:38 -0400

    simple HTTP homework

---
+++ src/imageResponses.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const meme = fs.readFileSync(`${__dirname}/../client/spongegar.png`);
+
+const getImage = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'image/png' });
+  response.write(meme);
+
+  response.end();
+};
+
+module.exports.getImage = getImage;
